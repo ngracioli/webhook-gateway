31b576c3b65412ef337e1cc987ddc4bee1c3f19b
ngracioli <nicolas.gracioli@gmail.com>
2026-01-30 23:55:48 -0300

    remove: prisma bettersqlite

--- src/infra/prisma.ts
+++ src/infra/prisma.ts
@@ -1,9 +1,7 @@
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from '@prisma/client';
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined');
 }
 
-const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
